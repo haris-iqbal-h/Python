710b66b4615504ed3926352d9d128d5d5d98a52f
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-06-04 23:02:39 +0500

    File Operations in Python

--- file/file.py
+++ file/file.py
@@ -15,3 +15,51 @@ with open('favorite_colors.csv') as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
         print(row)
+
+f = open("demofile.txt", "r")
+print(f.read())
+
+f = open("demofile.txt", "r")
+print(f.read(5))
+
+f = open("demofile.txt", "r")
+print(f.readline())
+
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+f = open("demofile.txt", "r")
+for x in f:
+    print(x)
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demofile3.txt", "r")
+print(f.read())
+
+f = open("myfile.txt", "x")
+
+f = open("myfile.txt", "w")
+
+
+import os
+os.remove("demofile.txt")
+
+if os.path.exists("demofile.txt"):
+    os.remove("demofile.txt")
+else:
+    print("The file does not exist")
+
+os.rmdir("myfolder")
