6a2f184d601b1943d9de88e36a3e2f2c1319ab93
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-06-06 23:06:40 +0500

    Shape and Array in Numpy

--- numpy/syntax.py
+++ numpy/syntax.py
@@ -43,7 +43,6 @@ print('Last element from 2nd dim: ', arr[1, -1])
 arr = np.array([1, 2, 3, 4, 5])
 x = arr.copy()
 arr[0] = 42
-
 print(arr)
 print(x)
 
@@ -51,14 +50,59 @@ print(x)
 arr = np.array([1, 2, 3, 4, 5])
 x = arr.view()
 arr[0] = 42
-
 print(arr)
 print(x)
 
 arr = np.array([1, 2, 3, 4, 5])
-
 x = arr.copy()
 y = arr.view()
-
 print(x.base)
 print(y.base)
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(arr.shape)
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print(arr)
+print('shape of array :', arr.shape)
+
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print(newarr)
+
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(2, 3, 2)
+print(newarr)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+print(arr.reshape(2, 4).base)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+newarr = arr.reshape(2, 2, -1)
+print(newarr)
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
+print(newarr)
+
+arr = np.array([1, 2, 3])
+
+for x in arr:
+    print(x)
+
+for x in arr:
+    print(x)
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+    for y in x:
+        for z in y:
+            print(z)
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+for idx, x in np.ndenumerate(arr):
+    print(idx, x)
