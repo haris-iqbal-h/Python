236ed8f40ef361eadb9f5256e1a7fdb71646bdd2
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-06-17 23:36:56 +0500

    Average Even Odd

--- examples/findSalary.py
+++
@@ -1,5 +0,0 @@
-newsalary = 0
-salary = input("enter new salary : ")
-raise = input("salary raise rate(%) : ")
-newsalary = int(salary)+(int(salary)*int(raise)/100)
-print("increased salary :",newsalary)
