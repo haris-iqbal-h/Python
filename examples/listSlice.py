0c39e4c019fd018db794596d062d074455300e59
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-07-14 23:04:41 +0500

    list slices

---
+++ examples/listSlice.py
@@ -0,0 +1,24 @@
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:])
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:])
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:2])
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:4])
+
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[1:4:2])
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[::-2])
